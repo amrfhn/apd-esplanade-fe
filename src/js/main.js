889ef04ffe688a5fc3e24c2ec43ec188d89c96f3
amirjefri <amir@apdgroup.com>
2019-06-26 11:47:50 +0800

    Update Nav Bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 import '../sass/main.scss'
+
+$(function() {
+
+
+    $('#hamb').on('click', function(){
+        $('.mm-wrapper').addClass('active');
+    })
+
+    $('.close-btn-x').on('click', function(){
+        $('.mm-wrapper').removeClass('active');
+    })
+
+
+
+});
