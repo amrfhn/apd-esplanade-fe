56f6714518f1d709f88a348e6545f46a96f3cde4
amirjefri <amir@apdgroup.com>
2019-06-28 10:26:12 +0800

    Update menu scrolling

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -7,10 +7,29 @@ $(function() {
         $('.mm-wrapper').removeClass('active');
     })
 
+    var $megaMenu = $('.mm-wrapper');
+
+    if($megaMenu.hasClass('active')){
+        console.log("active")
+        $('body').css('position','fixed');
+        $('body').css('overflow','hidden');
+
+    }
+
     $('#searchBar').modal({
         backdrop: false,
         show: false,
         focus: false
     });
+
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+
+        var $navTabs = $('#navTab');
+
+        if(!$navTabs.hasClass('nav-tabs')){
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        }
+      });
 
 });
