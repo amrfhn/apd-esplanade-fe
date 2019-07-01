7fa389f285af90d157635bc6eec1354e3a72a672
unknown <jingli@apdgroup.com>
2019-07-01 12:22:57 +0800

    updated video carousel

--- src/js/components/tabs-filter.js
+++ src/js/components/tabs-filter.js
@@ -4,6 +4,7 @@ $(function () {
         slidesToScroll: 1,
         arrows: true,
         infinite: false,
+        focusOnSelect: true,
         prevArrow:"<img class='a-left control-c prev slick-prev prev-tabs' src='./assets/img/icons/Previous.svg'>",
         nextArrow:"<img class='a-right control-c next slick-next next-tabs' src='./assets/img/icons/Next.svg'>"  
     });
