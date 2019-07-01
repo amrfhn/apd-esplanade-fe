65a39dff49eb93dc36063716f21a12105e6dd32d
amirjefri <amir@apdgroup.com>
2019-07-01 12:27:41 +0800

    update category slide tab

--- src/js/components/tabs-filter.js
+++ src/js/components/tabs-filter.js
@@ -4,7 +4,33 @@ $(function () {
         slidesToScroll: 1,
         arrows: true,
         infinite: false,
+        variableWidth: true,
         prevArrow:"<img class='a-left control-c prev slick-prev prev-tabs' src='./assets/img/icons/Previous.svg'>",
-        nextArrow:"<img class='a-right control-c next slick-next next-tabs' src='./assets/img/icons/Next.svg'>"  
+        nextArrow:"<img class='a-right control-c next slick-next next-tabs' src='./assets/img/icons/Next.svg'>" ,
+        afterChange: function(slider, index){
+            if (index == 0){
+                $('.slick-prev').css('display', 'none')
+            }
+            else{
+                $('.slick-prev').css('display', 'block')
+            }
+        },
+        responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            } 
+        ],
+        
     });
 });
