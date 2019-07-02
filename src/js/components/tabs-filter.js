edabb7f807f47686168f98113f6cd432610a8dc3
Jing Li <jingli@growthops.asia>
2019-07-02 15:03:45 +0800

    updated hero banner imange componenet

--- src/js/components/tabs-filter.js
+++ src/js/components/tabs-filter.js
@@ -18,6 +18,13 @@ $(function () {
     },
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
