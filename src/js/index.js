553ed7dc85e4a53d3f1d29701cba1c173b37bcf3
unknown <amirjefri>
2019-07-04 11:00:11 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -25,108 +25,108 @@ $(function () {
 
 
 
-var filterComponent = (function($) {
+// var filterComponent = (function($) {
 
-    var context = {
-        articles: [],
-        filters: [],
-        totalPages: 10,
-        pageSize: 10,
-        currPage: 1,
-        params: [],
-        $el: null
-    }
+//     var context = {
+//         articles: [],
+//         filters: [],
+//         totalPages: 10,
+//         pageSize: 10,
+//         currPage: 1,
+//         params: [],
+//         $el: null
+//     }
 
-    var initialize = function() {
+//     var initialize = function() {
     
         
 
-        // bind all the event listeners
-        $(".filter-item").click(function(e) {
+//         // bind all the event listeners
+//         $(".filter-item").click(function(e) {
 
-            var category = $(e.currentTarget).data("category");
-            context.category = category;
+//             var category = $(e.currentTarget).data("category");
+//             context.category = category;
 
-        })
-        $(".filter-genre").click(function(e) {
+//         })
+//         $(".filter-genre").click(function(e) {
 
-            var genre = $(e.currentTarget).data("key");
-            context.filters.genre = genre;
-            console.log("fire")
-            fireApi();
-        })
-    }
+//             var genre = $(e.currentTarget).data("key");
+//             context.filters.genre = genre;
+//             console.log("fire")
+//             fireApi();
+//         })
+//     }
 
-    var fireApi = function() {
-        var host = "http://dev.esplanade.growthopsapp.com/sitecore/api/offstage/articles";
+//     var fireApi = function() {
+//         var host = "http://dev.esplanade.growthopsapp.com/sitecore/api/offstage/articles";
 
 
-        $.ajax({
-            type: "GET",
-            // url: local,
-            url: host + "/eta/" + context.filters.genre + "/" + context.currPage + "/" + context.pageSize,
-            data: $.param(context.params),
-            // cache: false,
-            dataType: "json"
-        }).done(function(data){
-            console.log(data);
-        }).fail(function(){
-            console.log("fail")
-        })
-    }
+//         $.ajax({
+//             type: "GET",
+//             // url: local,
+//             url: host + "/eta/" + context.filters.genre + "/" + context.currPage + "/" + context.pageSize,
+//             data: $.param(context.params),
+//             // cache: false,
+//             dataType: "json"
+//         }).done(function(data){
+//             console.log(data);
+//         }).fail(function(){
+//             console.log("fail")
+//         })
+//     }
 
     
 
-    // DOM is ready and jQuery is loaded
-    $(function() {
-        var params = {
-            "browse": "ddygsyfs",
-            "contentType": "uydsgfysgfys",
-            "timeTaken": "ssfgsdyfg",
-            "sort": "trending"
-        }
+//     // DOM is ready and jQuery is loaded
+//     $(function() {
+//         var params = {
+//             "browse": "ddygsyfs",
+//             "contentType": "uydsgfysgfys",
+//             "timeTaken": "ssfgsdyfg",
+//             "sort": "trending"
+//         }
 
-        var filter = {
-            "genre": "all",
-            "pageNumber": "1",
-            "pageLimit": "10"
-        }
+//         var filter = {
+//             "genre": "all",
+//             "pageNumber": "1",
+//             "pageLimit": "10"
+//         }
     
-        // console.log($.param(params))
-        var host = "http://dev.esplanade.growthopsapp.com/sitecore/api/offstage/articles";
-        var local = "./assets/microsites/offstage/data/sample.json"
+//         // console.log($.param(params))
+//         var host = "http://dev.esplanade.growthopsapp.com/sitecore/api/offstage/articles";
+//         var local = "./assets/microsites/offstage/data/sample.json"
 
-        context.$el = $(".filter-component");
+//         context.$el = $(".filter-component");
 
-        var reqOne = $.ajax({
-            type: "GET",
-            // url: local,
-            url: host + "/eta/" + filter.genre + "/" + filter.pageNumber + "/" + filter.pageLimit,
-            data: $.param(params),
-            // cache: false,
-            dataType: "json"
-        }).done(function(data){
-            console.log(data);
-        }).fail(function(){
-            console.log("fail")
-        })
+//         var reqOne = $.ajax({
+//             type: "GET",
+//             // url: local,
+//             url: host + "/eta/" + filter.genre + "/" + filter.pageNumber + "/" + filter.pageLimit,
+//             data: $.param(params),
+//             // cache: false,
+//             dataType: "json"
+//         }).done(function(data){
+//             console.log(data);
+//         }).fail(function(){
+//             console.log("fail")
+//         })
 
 
 
-        // var reqTwo = $.ajax({
+//         // var reqTwo = $.ajax({
 
-        // })
+//         // })
 
-        $.when(reqOne)
-        .done(function(data) {
-            context.articles = data[0].articles;
-            context.filters = data[1];
+//         $.when(reqOne)
+//         .done(function(data) {
+//             context.articles = data[0].articles;
+//             context.filters = data[1];
 
-            initialize();
-        })
-    })
+//             initialize();
+//         })
+//     })
 
-    return {
-        context
-    }
+//     return {
+//         context
+//     }
 })(jQuery);
