c3501276248208ef6edaeada857dc173236fbc04
Jing Li <jingli@growthops.asia>
2019-07-10 14:01:05 +0800

    Fixed video modal gradient issue and updated image component resizing

--- src/js/components/video-modal.js
+++ src/js/components/video-modal.js
@@ -1,8 +1,18 @@
 $(function () {
-    $("#video-modal").on("hide.bs.modal", function (e) {
+    var videoModal = $("#video-modal");
+
+    videoModal.on("hide.bs.modal", function (e) {
         var $if = $(e.delegateTarget).find('iframe');
         var src = $if.attr("src");
         $if.attr("src", '/empty.html');
         $if.attr("src", src);
+    })
+    
+    videoModal.on("hidden.bs.modal", function (e) {
+        $('.nav-bar-wrapper').css('padding-right', 'unset')
+    })
+
+    videoModal.on('show.bs.modal', function () {
+        $('.nav-bar-wrapper').css('padding-right', '17px')
     })
 })
