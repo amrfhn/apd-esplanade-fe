fc76259d3531b6acbda1c67ad9ee2a26ac6a99f6
Amir Farhan Jefri <amir@apdgroup.com>
2019-07-12 15:42:22 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -14,11 +14,7 @@ $(function () {
     $('.thumbnail-holder').each(function() {
         var mobileImage = $(this).attr('data-mobile-image')
         var desktopImage = $(this).attr('data-desktop-image')
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> 1d01d175783e2d6d397b6d2cfdc136ab1d92dcaf
         if(width <= 768)
         {  
             if(mobileImage !== ""){
@@ -37,11 +33,6 @@ $(function () {
         var carouselMobileImage = $(this).attr('data-mobile-image')
         var carouselDesktopImage = $(this).attr('data-desktop-image')
     
-<<<<<<< HEAD
-=======
-        console.log(carouselMobileImage);
-    
->>>>>>> 1d01d175783e2d6d397b6d2cfdc136ab1d92dcaf
         if(width <= 768)
         {  
             if(carouselMobileImage !== ""){
