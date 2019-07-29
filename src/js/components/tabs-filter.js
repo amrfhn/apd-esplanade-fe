937f31e698c31b443533e4f48f51f34b6274e04d
Chuan Theng <ctan@apdgroup.com>
2019-07-29 19:20:22 +0800

    highlighted tab

--- src/js/components/tabs-filter.js
+++ src/js/components/tabs-filter.js
@@ -73,8 +73,11 @@ $(function () {
                 $('#genreTabs').find('#' + dataKey).click();
 
                 _this.filterGenre(dataKey);
+                $('.genre-tabs .wrapper').animate({
+                    scrollLeft: $('.genre-tabs .active').position().left 
+                }, 2000);
             })
-
+ 
 
         },
         updated: function () {
