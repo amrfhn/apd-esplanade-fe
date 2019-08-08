933dce9219f5db7c863dda9c44242437340725df
Chuan Theng <ctan@apdgroup.com>
2019-08-08 10:41:11 +0800

    progress bar js update

--- src/js/components/progress-bar.js
+++ src/js/components/progress-bar.js
@@ -1,43 +1,46 @@
 $(function (){
-
-    var target;
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    "use strict";
     
-    $('.progress-wrapper').each(function () {
-        if(width <= 768) {
-            target = '.progress-wrapper.d-block';
-        } 
-        if(width > 768) {
-            target = '.progress-wrapper.d-md-block';
-        }
-    })
-
-    updateProgressBar(target);
-
-    window.onscroll = function() {updateProgressBar(target)};
-
-    // run sticky progress bar
-    var $fixProgress = $('.progress-wrapper');
-    if($fixProgress.length>0){
-        $(document).on('scroll', function() {
-            
-            let scrollPos = $(this).scrollTop()
-            let menuHeight = $('.left-wrapper').height();
-            
-            if(scrollPos > ($(target).offset().top - menuHeight)) {
-            $(target).find(".progress-container").addClass("fixedbar");
-            } else {
-                $(target).find(".progress-container").removeClass("fixedbar"); 
+    if($('.progress-wrapper').length > 0){
+        var target;
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        
+        $('.progress-wrapper').each(function () {
+            if(width <= 768) {
+                target = '.progress-wrapper.d-block';
             } 
+            if(width > 768) {
+                target = '.progress-wrapper.d-md-block';
+            }
+        })
 
-        });
-    }
+        updateProgressBar(target);
+
+        window.onscroll = function() {updateProgressBar(target)};
+
+        // run sticky progress bar
+        var $fixProgress = $('.progress-wrapper');
+        if($fixProgress.length>0){
+            $(document).on('scroll', function() {
+                
+                let scrollPos = $(this).scrollTop()
+                let menuHeight = $('.left-wrapper').height();
+                
+                if(scrollPos > ($(target).offset().top - menuHeight)) {
+                $(target).find(".progress-container").addClass("fixedbar");
+                } else {
+                    $(target).find(".progress-container").removeClass("fixedbar"); 
+                } 
+
+            });
+        }
 
 
-    function updateProgressBar(selector) {
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
-        document.querySelectorAll(selector + ' .progress-bar')[0].style.width = scrolled + "%";
+        function updateProgressBar(selector) {
+            var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+            var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            var scrolled = (winScroll / height) * 100;
+            document.querySelectorAll(selector + ' .progress-bar')[0].style.width = scrolled + "%";
+        }
     }
 });
