d7b588d318cb541a2ba7f462f4865b89896ded13
Amir Farhan Jefri <amir@apdgroup.com>
2019-08-19 16:02:19 +0800

    Tag cloud results

--- src/js/components/tag-result.js
+++ src/js/components/tag-result.js
@@ -7,13 +7,16 @@ $(function () {
         var md = window.matchMedia('(min-width: 769px)');
 
         var host = 'http://dev.esplanade.growthopsapp.com/';
+        var currUrl = window.location.href;
 
         var data = {
             message: 'Tag Result Vue',
             tag: '',
-            results: '',
-            pageNum: '',
-            pageSize: ''
+            pageNum: 1,
+            pageSize: 10,
+            results: [],
+            keyword: '',
+            fetchingData: false
         }
 
         var params = {
@@ -23,6 +26,10 @@ $(function () {
 
         }
 
+        var url = new URL(currUrl);
+        var query_string = url.search;
+        var searchParams = new URLSearchParams(query_string);
+
         var tagApp = new Vue({
             el: '#tagResult',
             data: data,
@@ -30,14 +37,73 @@ $(function () {
             mounted: function () {
                 var _this = this;
 
+                this.checkUrl();
+                this.fetchResults();
+
             },
 
             methods: {
+
+                checkUrl: function (){
+
+                    if(currUrl.indexOf('tag') > -1){
+                        var tagValue = url.searchParams.get('tag');
+                        data.tag = tagValue;
+                    }
+
+                },
                 
-                
+                fetchResults: function (){
+
+                    var _this = this;
+                    var url = host + '/sitecore/api/offstage/tagcloud/results/' + this.tag + '/' + this.pageNum + '/' + this.pageSize;
+
+                    var request = $.ajax({
+                        type: 'GET',
+                        url: url,
+                        dataType: 'json',
+                        data: decodeURIComponent($.param(params))
+
+                    }).done(function (data){
+
+                        _this.results = data.TagCloudResult
+                        _this.keyword = data.Keyword
+                        
+                        if(data.TagCloudResult.length < 10 || data.TagCloudResult.length == 0 ){
+                            $('#loadMore').parent().addClass('d-none').removeClass('d-flex');
+                        } else {
+                            $('#loadMore').parent().addClass('d-flex').removeClass('d-none');
+                        }
+                    })
+                },
+
+                loadMoreResults: function (){
+
+                    var _this = this;
+                    var offset = 10;
+
+                    this.pageNum += 1;
+
+                    var loadUrl = host + '/sitecore/api/offstage/tagcloud/results/' + this.tag + '/' + this.pageNum + '/' + offset;
+
+                    this.fetchingData = true;
+
+                    var loadData = $.ajax({
+                        type: 'GET',
+                        url: loadUrl,
+                        dataType: 'json',
+                        data: $.param(params) 
+                    }).done(function (data){
+                        _this.results = _this.results.concat(data.TagCloudResult);
+
+                        if (data.TagCloudResult.length < offset || data.TagCloudResult.length == 0) {
+                            $('#loadMore').parent().addClass('d-none').removeClass('d-flex');
+                        }
+
+                        _this.fetchingData = false;
+                    })
+                }
             },
-
-
 
         })
     }
