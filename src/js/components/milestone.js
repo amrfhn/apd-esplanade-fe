15b7cb406a3bc6ce992677d23dd1219280a50ee6
Jing Li <jingli@growthops.asia>
2019-08-28 12:07:01 +0800

    updated milestone component

--- src/js/components/milestone.js
+++ src/js/components/milestone.js
@@ -1,29 +1,26 @@
 $(function () {
 
     'use strict'
-
+    
     var $milestoneWrap = $('#milestones-wrapper');
     var milestone = $milestoneWrap.children();
     var btnLoadMore = $('#loadMilestone'); 
 
-    if(milestone.length < 4){
-        btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
-    } else {
-        btnLoadMore.parent().addClass('d-flex').removeClass('d-none')
-    }
-
-    btnLoadMore.on('click', function(){
-        btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
-    })
-
-    $milestoneWrap.each(function(index){
-        
-        var $lastItem = $(this).children().last().children().last()
-
-        if($lastItem.find('.box-left')){
-            $('.box-left::before').css('content', 'none')
+    if($milestoneWrap.length > 0) {
+        if(milestone.length < 4){
+            btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
         } else {
-
+            btnLoadMore.parent().addClass('d-flex').removeClass('d-none')
         }
-    })
+    
+        btnLoadMore.on('click', function(){
+            btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
+        })
+    
+        $milestoneWrap.each(function(){
+            if(!$(this).children().hasClass('more-miles')){
+                $(this).children().addClass('milestones-single-item')
+            } 
+        })
+    }
 })
