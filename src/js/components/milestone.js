67fb69795f40d0ae92b45d925e01d60c50230d35
Jing Li <jingli@growthops.asia>
2019-08-28 15:53:43 +0800

    updated milestone component

--- src/js/components/milestone.js
+++ src/js/components/milestone.js
@@ -2,25 +2,50 @@ $(function () {
 
     'use strict'
     
-    var $milestoneWrap = $('#milestones-wrapper');
+    var $milestoneWrap = $('.milestones-wrapper');
     var milestone = $milestoneWrap.children();
-    var btnLoadMore = $('#loadMilestone'); 
 
     if($milestoneWrap.length > 0) {
-        if(milestone.length < 4){
-            btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
-        } else {
-            btnLoadMore.parent().addClass('d-flex').removeClass('d-none')
-        }
+        // if(milestone.length < 4){
+        //     btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
+        // } else {
+        //     btnLoadMore.parent().addClass('d-flex').removeClass('d-none')
+        // }
     
-        btnLoadMore.on('click', function(){
-            btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
-        })
+        // btnLoadMore.on('click', function(){
+        //     btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
+        // })
     
-        $milestoneWrap.each(function(){
+        $milestoneWrap.each(function(index, item){
+            var $btnLoadMore = $(this).find('#loadMilestone');
+
             if(!$(this).children().hasClass('more-miles')){
                 $(this).children().addClass('milestones-single-item')
-            } 
+            } else {
+               var moreMiles =  $(this).find('.more-miles').attr('id');
+               var newID = moreMiles + "-" + index
+               var replaceID =  $(this).find('.more-miles').attr('id', newID);
+               console.log(replaceID)
+
+               var buttonTarget = $btnLoadMore.attr('data-target')
+               var newTarget = buttonTarget + "-" + index
+               var replaceTarget = $btnLoadMore.attr('data-target', newTarget)
+
+               console.log(replaceTarget)
+            }
+
+            if($(this)[0].children.length < 4){
+                $btnLoadMore.parent().removeClass('d-flex').addClass('d-none')
+            } else {
+                $btnLoadMore.parent().addClass('d-flex').removeClass('d-none')
+            }
+        
+            $btnLoadMore.on('click', function(){
+                $(this).parent().removeClass('d-flex').addClass('d-none')
+            })            
+
+            console.log($(this)[0].children.length)
+
         })
     }
 })
